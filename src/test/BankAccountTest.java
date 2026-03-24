2e2a92300834457a3b215ccf937c878567ef12eb
Birch-Yang <birch.y@wustl.edu>
2026-03-24 10:39:45 -0500

    add test

--- src/test/BankAccountTest.java
+++ src/test/BankAccountTest.java
@@ -4,6 +4,7 @@ import main.BankAccount;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -24,6 +25,39 @@ public class BankAccountTest {
             fail();
         } catch (IllegalArgumentException e) {
             //do nothing, test passes
+        }
+    }
+
+    @Test
+    public void testCloseAccount() {
+        BankAccount testAccount = new BankAccount();
+        testAccount.closeAccount();
+        assertTrue(testAccount.isClosed());
+
+        try {
+            testAccount.closeAccount();
+            fail();
+        } catch (IllegalStateException e) {
+          //nothing
+        }
+    }
+
+    @Test
+    public void testTransfer() {
+        BankAccount fromAccount = new BankAccount();
+        BankAccount toAccount = new BankAccount();
+
+        fromAccount.deposit(100);
+        fromAccount.transferTo(toAccount, 40);
+
+        assertEquals(60, fromAccount.getBalance(), 0.01);
+        assertEquals(40, toAccount.getBalance(), 0.01);
+
+        try {
+            fromAccount.transferTo(toAccount, 100);
+            fail();
+        } catch (IllegalArgumentException e) {
+          //nothing
         }
     }
 }
