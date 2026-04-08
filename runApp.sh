dd7ccfb44d0a4e2e5e09b2b2b06f566d29f11c99
Birch-Yang <birch.y@wustl.edu>
2026-04-07 22:39:37 -0500

    update runApp.sh

--- runApp.sh
+++ runApp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#put commands here to compile and run your app from command line
+set -e
 
-javac src/main/*.java
-java src/main/MainMenu.java
+mkdir -p bin
+javac -d bin src/main/*.java
+java -cp bin main.MainMenu
